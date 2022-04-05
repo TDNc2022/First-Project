0c0cdb32ff8ed09c6d235282406bb1913b0b8692
Watona <fran.bazaesg@alumnos.arcos.cl>
2022-04-05 18:12:40 -0400

    TestingSceneWatona

--- Unity Project/Assets/Scripts/Enemy/IA_Enemy.cs
+++ Unity Project/Assets/Scripts/Enemy/IA_Enemy.cs
@@ -1,20 +1,22 @@
-using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace FirstProject{
 public class IA_Enemy : MonoBehaviour
-{
-    //private Vector3 target;
-    // Start is called before the first frame update
-    void Start()
     {
+        public Vector3 target;
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+            target = PlayerController.Instance.transform.position;
+        }
+
     }
 }
--- Unity Project/Assets/Scripts/Enemy/Spawn_Enemy.cs
+++ Unity Project/Assets/Scripts/Enemy/Spawn_Enemy.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace FirstProject{
+
 public class Spawn_Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        public GameObject enemyPrefab;
+        GameObject freshEnemy;
+        public Vector3 spTarget;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public float timer;
+        // Start is called before the first frame update
+        void Start()
+        {
+            timer = 1;
+            spTarget = PlayerController.Instance.transform.position;
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            Timer();
+        }
+        public void Spawn(){
+            
+            freshEnemy = Instantiate(enemyPrefab,transform.position,transform.rotation);
+        }
+        public void Timer(){
+            timer -= Time.deltaTime;
+            if(timer <= 0){
+                Spawn();
+                timer = 10;
+            }
+        }
     }
 }
