051a98954e6aa040985c6db988935f145c3b2d35
Watona <fran.bazaesg@alumnos.arcos.cl>
2022-04-05 17:17:57 -0400

    xd

---
+++ Unity Project/Assets/Scripts/Enemigo/Spawn_Enemigo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Enemigo : MonoBehaviour
+{
+    Vector3 player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void GetTarget(){
+        //player = GetComponent<PlayerController>().transform.position;
+    }
+}
